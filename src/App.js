c5ccce6bdec7a30cacacb80bd612ef5b04b5cccd
Sumio K <tap.miwo@gmail.com>
2022-10-11 07:07:30 +0900

    have an error

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import SignIn from './components/SignIn';
+import {useAuthState} from "react-firebase-hooks/auth";
+import {auth} from "./firebase.js"
+import Line from './components/Line';
+
 
 function App() {
+  const user = useAuthState(auth);
+
   return (
     <div className="App">
-      <SignIn />
+      {user ? <Line /> : <SignIn />}
     </div>
   );
 }
